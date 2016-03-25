da8777fab88b769ccb2cc3a860b7cf2c92b7723e
will van wart <william@wvwproductions.com>
2016-03-25 12:58:40 -0400

    add users/controller and new view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: true
 
   has_many :questions
   has_many :answers
-  has_many :comments    #, as: :commentable
+  has_many :comments
   has_many :votes
+
+  has_secure_password
 end
