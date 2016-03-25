ae6fb8326c3c219422fec53f747f35b5da1239a5
will van wart <william@wvwproductions.com>
2016-03-25 13:27:53 -0400

    add questions controller logic

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,32 @@
+class QuestionsController < ApplicationController
+
+  def index
+    @question = Question.all
+  end
+
+  def new
+    @question = Question.new
+    @question.answers.build
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:success] = "question created successfully"
+      redirect_to questions_path
+    else
+      render 'questions/new'
+    end
+  end
+
+  private
+
+    def question_params
+      params.require(:question).permit(:title, :content).merge(user_id: current_user.id)
+    end
+
+end
