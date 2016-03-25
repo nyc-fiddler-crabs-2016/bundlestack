204918efbf51fe88e656186c5a67ea3d8026e84d
Matthew Oppenheimer & Will Van Wart <pair+mattopps.williej@devbootcamp.com>
2016-03-25 17:08:14 -0400

    write helper method. edit seed file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :authorize_access
 
   def current_user
     User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def authorize_access(object)
+    halt 403 unless current_user == object.user.id
   end
 
 end
