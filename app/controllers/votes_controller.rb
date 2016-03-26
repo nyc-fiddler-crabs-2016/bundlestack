9a52b6c6602ff8397a7dd58fec998726b4fd49e6
Matthew Oppenheimer <mattopps@gmail.com>
2016-03-26 12:25:01 -0400

    Fix vote up and down actions. rewrite routes as singular routes withdynamic votable id and type parameters in link paths to be added as parameter queries. add _path helpers. Delete unneccesary create and destroy actions

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,37 +1,12 @@
 class VotesController < ApplicationController
 
   def up
-    binding.pry
-    @object = Vote.find_or_create_by(user_id, votable_type, votable_id)
-    @object.vote.increment
-  #   if already_voted
-  #     redirect_to votes_path
-  #   else
-  #     @object = (user_id: current_user.id, votable_id: params[:] )
-  #     @vote.increment
-  # end
+    @vote = Vote.find_or_create_by(user_id: current_user.id, votable_id: params[:votable_id], votable_type:params[:votable_type])
+    @vote.increment
+  end
 
   def down
-    @object = Vote.find_or_create_by(user_id, votable_type, votable_id)
-    @object.vote.decrement
+   @vote = Vote.find_or_create_by(user_id: current_user.id, votable_id: params[:votable_id], votable_type:params[:votable_type])
+    @vote.decrement
   end
-
-  # def create
-  #   @vote = find_or_create_by(user_id: current_user.id, votable_id: )
-  #   if already_voted
-  #     redirect_to vote_path, method: :delete
-  #   else
-  #     @vote = Vote.new(vote_param)
-  #   end
-  # end
-
-  # def destroy
-  # end
-
-  private
-
-  def vote_param
-    params.require(:vote).permit(:value).merge(user_id: current_user.id)
-  end
-
 end
