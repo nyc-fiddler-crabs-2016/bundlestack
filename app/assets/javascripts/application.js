5d641d2063d355be581af1e9a5a59a0634d3d082
Thomas Yancey <email": null>
2016-03-27 14:13:50 -0400

    Add close ajax for vote on at least questions page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,18 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$( document ).ready(function() {
+  $('.upvote').on ('click', function(e){
+    alert('ya');
+    event.preventDefault();
+    var voteObj = event.currentTarget
+    var url = event.currentTarget.href;
+    debugger
+    $.ajax({
+      url: url,
+      data: $(this).serialize()
+    }).done(function(response){
+    debugger
+    });
+  });
+});
