5884fe98a645b8a99df6eed0db0fbfd4c05cfc94
Karen Ball & Thomas Yancey <pair+greensneakers.thomas-yancey@devbootcamp.com>
2016-03-26 21:32:20 -0400

    Add working favorite ability for owner of question, should move some of logic to model from controller

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,14 @@
 class Answer < ActiveRecord::Base
 
-  include Valueable
+  def question_owner_id
+    self.question.user_id
+  end
 
   belongs_to :user
   belongs_to :question
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
+
+  private
+
 end
